ee618b642bb46097025c7300c88265f36aad03c0
= <26445308+GingaaBread@users.noreply.github.com>
2023-11-19 19:52:53 +0100

    Shrink & Grow states

--- Assets/Scripts/main/view/CardInHandContainer.cs
+++ Assets/Scripts/main/view/CardInHandContainer.cs
@@ -8,15 +8,18 @@ namespace main.view
 {
     public class CardInHandContainer : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
-        private const float CLAMP_WIDTH = 20f, CLAMP_HEIGHT = 10f;
+        private const float CLAMP_WIDTH = 20f, CLAMP_HEIGHT = 10f, PLAY_HEIGHT_LIMIT = -6.5f;
         [SerializeField] private CardView _cardViewPrefab;
+        [SerializeField] private Animator _animator;
 
         private CardView _child;
         private RectTransform _childRectTransform;
+        private CardPlayState _playState;
 
         public void OnBeginDrag(PointerEventData eventData)
         {
             PlayerHandCanvas.Instance.SetAsDirectChild(_child.transform);
+            _playState = CardPlayState.UNPLAYABLE;
         }
 
         public void OnDrag(PointerEventData eventData)
@@ -28,6 +31,16 @@ namespace main.view
 
             _childRectTransform.position = new Vector3(Mathf.Clamp(pos.x, -CLAMP_WIDTH, CLAMP_WIDTH),
                 Mathf.Clamp(pos.y, -CLAMP_HEIGHT, CLAMP_HEIGHT));
+
+            var lastPlayState = _playState;
+
+            _playState = pos.y >= PLAY_HEIGHT_LIMIT ? CardPlayState.PLAYABLE : CardPlayState.UNPLAYABLE;
+
+            if (lastPlayState == _playState) return;
+
+            _animator.Play(_playState is CardPlayState.PLAYABLE
+                ? "CardInHandContainer_Shrink"
+                : "CardInHandContainer_Expand");
         }
 
         public void OnEndDrag(PointerEventData eventData)
@@ -41,6 +54,14 @@ namespace main.view
 
             _child = newCardView;
             _childRectTransform = _child.GetComponent<RectTransform>();
+            _playState = CardPlayState.IDLE;
+        }
+
+        private enum CardPlayState
+        {
+            PLAYABLE,
+            UNPLAYABLE,
+            IDLE
         }
     }
 }
