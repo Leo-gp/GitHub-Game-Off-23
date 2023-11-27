18724757f3c6b5efda7821f55e6c9808946cd9ce
= <26445308+GingaaBread@users.noreply.github.com>
2023-11-27 01:53:37 +0100

    Draw animation

--- Assets/Scripts/main/view/CardView.cs
+++ Assets/Scripts/main/view/CardView.cs
@@ -11,7 +11,7 @@ namespace main.view
 {
     public class CardView : MonoBehaviour
     {
-        private const float DISCARD_SPEED = 3.5f, CURVE_HEIGHT = 21f;
+        private const float DISCARD_SPEED = 3.5f, DISCARD_CURVE_HEIGHT = 21f, DRAW_CURVE_HEIGHT = 5f, DRAW_SPEED = 5f;
 
         [SerializeField] private TMP_Text _cardNameText;
         [SerializeField] private TMP_Text _cardClassText;
@@ -24,14 +24,41 @@ namespace main.view
         private Sprite _actionCardPanelSprite;
 
         [SerializeField] private Sprite _itemCardPanelSprite;
-
         private Vector3[] _bezierNodes;
         private float _bezierTargetCount;
-        private bool _isBeingDiscarded;
+        private bool _isBeingDiscarded, _isBeingDrawn;
+
+        private Transform _transform, _parent;
+
+        public RectTransform RectTransform => _transform as RectTransform;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
 
         private void Update()
         {
+            HandlePotentialDraw();
             HandlePotentialDiscard();
+        }
+
+        private void HandlePotentialDraw()
+        {
+            if (!_isBeingDrawn) return;
+
+            if (_bezierTargetCount >= 1f)
+            {
+                _isBeingDrawn = false;
+                _transform.SetParent(_parent);
+                _transform.position = Vector3.zero;
+            }
+
+            _bezierTargetCount += DRAW_SPEED * Time.deltaTime;
+
+            var m1 = Vector3.Lerp(_bezierNodes[0], _bezierNodes[1], _bezierTargetCount);
+            var m2 = Vector3.Lerp(_bezierNodes[1], _bezierNodes[2], _bezierTargetCount);
+            _transform.position = Vector3.Lerp(m1, m2, _bezierTargetCount);
         }
 
         private void HandlePotentialDiscard()
@@ -44,7 +71,7 @@ namespace main.view
 
             var m1 = Vector3.Lerp(_bezierNodes[0], _bezierNodes[1], _bezierTargetCount);
             var m2 = Vector3.Lerp(_bezierNodes[1], _bezierNodes[2], _bezierTargetCount);
-            transform.position = Vector3.Lerp(m1, m2, _bezierTargetCount);
+            _transform.position = Vector3.Lerp(m1, m2, _bezierTargetCount);
         }
 
         public void Discard()
@@ -54,9 +81,27 @@ namespace main.view
             _bezierTargetCount = 0f;
 
             _bezierNodes = new Vector3[3];
-            _bezierNodes[0] = transform.position;
+            _bezierNodes[0] = _transform.position;
             _bezierNodes[2] = PlayerHandCanvas.Instance.DiscardTargetPosition;
-            _bezierNodes[1] = _bezierNodes[0] + (_bezierNodes[2] - _bezierNodes[0]) / 2 + Vector3.up * CURVE_HEIGHT;
+            _bezierNodes[1] = _bezierNodes[0] + (_bezierNodes[2] - _bezierNodes[0]) / 2 +
+                              Vector3.up * DISCARD_CURVE_HEIGHT;
+        }
+
+        public void HandleDraw([NotNull] Transform parent)
+        {
+            _parent = parent;
+
+            // We will draw from the same position used for discarding cards
+            var drawOriginPosition = PlayerHandCanvas.Instance.DiscardTargetPosition;
+
+            _transform.position = drawOriginPosition;
+            _isBeingDrawn = true;
+
+            _bezierNodes = new Vector3[3];
+            _bezierNodes[0] = drawOriginPosition;
+            _bezierNodes[2] = parent.position;
+            _bezierNodes[1] = _bezierNodes[0] + (_bezierNodes[2] - _bezierNodes[0]) / 2 +
+                              Vector3.up * DRAW_CURVE_HEIGHT;
         }
 
         public void Render([NotNull] Card cardEntity)
